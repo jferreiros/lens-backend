88beb2064fde88692229d745c958c5fdf0de78ac
joaquinferreiros <jferreiros@emailingnetwork.com>
2024-01-23 16:13:27 +0100

    Initial commit

---
+++ test/lens-backend-meetoptics.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as LensBackendMeetoptics from '../lib/lens-backend-meetoptics-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/lens-backend-meetoptics-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new LensBackendMeetoptics.LensBackendMeetopticsStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
